fa49f443f18b70b11dbf84bbe631f2cb618afbfe
Vaugham Hong <vaughamhong@gmail.com>
2014-10-14 17:02:49 -0700

    reverted to minizip compatible with current zlib

--- Makefile
+++ Makefile
@@ -3,7 +3,6 @@ CFLAGS=-O -I../..
 
 UNZ_OBJS = miniunz.o unzip.o ioapi.o ../../libz.a
 ZIP_OBJS = minizip.o zip.o   ioapi.o ../../libz.a
-TEST_FILES = test.zip readme.old readme.txt
 
 .c.o:
 	$(CC) -c $(CFLAGS) $*.c
@@ -16,19 +15,11 @@ miniunz:  $(UNZ_OBJS)
 minizip:  $(ZIP_OBJS)
 	$(CC) $(CFLAGS) -o $@ $(ZIP_OBJS)
 
-.PHONY: test clean
-
 test:	miniunz minizip
-	@rm -f $(TEST_FILES)
-	@cp README.md readme.txt
-	@touch -t 200712301223.44 readme.txt
-	./minizip test.zip readme.txt
+	./minizip test readme.txt
 	./miniunz -l test.zip
 	mv readme.txt readme.old
 	./miniunz test.zip
-	@diff -q README.md readme.txt || echo "Test failed: files differ"
-	@[[ "$$(stat -c %Y readme.txt)" = "$$(stat -c %Y readme.old)" ]] || echo "Test failed: timestamp not preserved"
-	@rm -f $(TEST_FILES)
 
 clean:
-	/bin/rm -f *.o *~ minizip miniunz $(TEST_FILES)
+	/bin/rm -f *.o *~ minizip miniunz
--- iowin32.h
+++ iowin32.h
@@ -1,23 +1,18 @@
 /* iowin32.h -- IO base function header for compress/uncompress .zip
-   Version 1.1, February 14h, 2010
-   part of the MiniZip project
+     Version 1.1, February 14h, 2010
+     part of the MiniZip project - ( http://www.winimage.com/zLibDll/minizip.html )
 
-   Copyright (C) 1998-2010 Gilles Vollant
-     http://www.winimage.com/zLibDll/minizip.html
-   Modifications for Zip64 support
-     Copyright (C) 2009-2010 Mathias Svensson
-     http://result42.com
+         Copyright (C) 1998-2010 Gilles Vollant (minizip) ( http://www.winimage.com/zLibDll/minizip.html )
 
-   This program is distributed under the terms of the same license as zlib.
-   See the accompanying LICENSE file for the full text of the license.
+         Modifications for Zip64 support
+         Copyright (C) 2009-2010 Mathias Svensson ( http://result42.com )
+
+         For more info read MiniZip_info.txt
+
 */
 
-#ifndef _IOWIN32_H
-#define _IOWIN32_H
-
-#ifdef WIN32
-
 #include <windows.h>
+
 
 #ifdef __cplusplus
 extern "C" {
@@ -30,8 +25,4 @@ void fill_win32_filefunc64W OF((zlib_filefunc64_def* pzlib_filefunc_def));
 
 #ifdef __cplusplus
 }
-#endif
-
-#endif
-
 #endif
